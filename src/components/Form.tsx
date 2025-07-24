98956050f7a0401290651836b178ab50e55c4ec9
Ariel Perez <ariperez@frba.utn.edu.ar>
2025-07-23 22:44:38 -0300

    #2-Refactor, new style and components. Use de reduce

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,26 @@
 import { useState } from "react";
+import type { Dispatch } from "react";
+import type { FormEvent } from "react";
 import type { ChangeEvent } from "react";
+import {v4 as uuidv4} from 'uuid'
 import type { Activity } from "../types";
 import { categories } from "../data/categories";
+import type { ActivityAction } from "../reducers/activityReducer";
+type FormProps = {
+  dispatch: Dispatch<ActivityAction>;
+};
 
-export default function Form() {
-  const [activity, setActivity] = useState<Activity>({
-    category: 1,
-    name: "",
-    calories: 0,
-  });
+const initialState:Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: "",
+  calories: 0,
+};
+export default function Form({ dispatch }: FormProps) {
+  const [activity, setActivity] = useState<Activity>(initialState);
 
   const isValid = () => {
-    const {name, calories} = activity;
+    const { name, calories } = activity;
     return name.length > 0 && calories > 0;
   }
   const handleChange = (
@@ -20,11 +29,20 @@ export default function Form() {
     const isNumberField = ['category', 'calories'].includes(e.target.id);
     setActivity({
       ...activity,
-      [e.target.id]: isNumberField? +e.target.value : e.target.value,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
     });
   };
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({
+      type: 'save-activity',
+      payload: { newActivity: activity },
+    });
+    setActivity({...initialState, id: uuidv4() });
+    
+  };
   return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg" onSubmit={handleSubmit}>
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className="font-bold">
           Categoria:
@@ -69,7 +87,7 @@ export default function Form() {
       <input
         type="submit"
         className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bol uppercase text-white cursor-pointer disabled:opacity-10"
-        value="Guardar comida o ejercicio"
+        value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Actividad'}
         disabled={!isValid()}
       />
     </form>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export type Category = { id: number; name: string };
 export type Activity = {
-  category: number;
-  name: string;
-  calories: number;
+  id: string
+  category: number
+  name: string
+  calories: number
 };
